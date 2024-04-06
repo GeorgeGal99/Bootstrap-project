c298e1437e88e7b5c277f57666006c0413c5b8ee
GeorgeGal99 <144837092+GeorgeGal99@users.noreply.github.com>
2024-04-06 16:22:40 +0300

    ok

--- Proiect 22 mai/proiect1_register.js
+++ Proiect 22 mai/proiect1_register.js
@@ -6,7 +6,7 @@ const dataNastere = document.querySelector("#dataNastere");
 let btnregister = document.querySelector("#btn__register");
 let regex = /^[a-zA-Z]+$/;
 document.getElementById("test_label").innerText = "";
-
+let user = [];
 
 // functie html
 let allInputs = document.querySelectorAll("input");
@@ -38,192 +38,134 @@ function focusOut(e) {
 
 
 
-btnregister.addEventListener('click', btn_register);
-function btn_register() {
-    valid = false
-    if (name.value.trim() == "") {
-        toastr["error"](" Name can't be blank");
-    } else if (!name.value.match(/^[a-zA-Z]+$/)) {
-        toastr["error"]("No numbers");
-    } else if ((name.value).length < 2) {
-        toastr["error"]("Name to short");
-    } else {
-        // validateInput(name);
-        toastr["success"]("Good");
+function validateInput() {
+
+    class User {
+        constructor(email, password, confirmPassword, name, dataNastere) {
+            this.email = email;
+            this.password = password;
+            this.confirmPassword = confirmPassword;
+            this.name = name;
+            this.dataNastere = dataNastere;
+            this.value = value;
+        }
     }
 
+    if (name.value.trim() == "") {
+        toastr["error"](" Name can't be blank");
+        valid = false;
+    } else if (!name.value.match(/^[a-zA-Z]+$/)) {
+        toastr["error"]("No numbers");
+        valid = false;
+    } else if ((name.value).length < 2) {
+        toastr["error"]("Name to short");
+        valid = false;
+    } else {
+        validateInput(name);
+        valid = true;
+        saveUserToLocalStorage();
+    }
+    mj
     if (registerEmail.value.trim() == "") {
+        valid = false;
         toastr["error"](" Mail can't be blank");
+
     } else if (!registerEmail.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+        valid = false;
         toastr["error"]("Email wrong format");
     } else {
-        // validateInput(registerEmail);
-        toastr["success"]("Good email");
+        valid = true;
+        validateInput(registerEmail);
+        saveUserToLocalStorage();
+
     }
 
 
     if (registerPassword.value.trim() == "") {
+        valid = false;
         toastr["error"]("Put password");
     } else if (!registerPassword.value.match(/^[A-Za-z]\w{5,14}$/)) {
+        valid = false;
         toastr["error"]("Password wrong format");
     } else {
-        // validationInput(registerPassword);
-        toastr["success"]("Good password");
+        valid = true;
+        validateInput(registerPassword);
+        saveUserToLocalStorage();
+
     }
 
 
     if (confirmPassword.value.trim() == "") {
+        validateInput(confirmPassword);
+        valid = false;
         toastr["error"]("Confirm password");
+    }
+    if (password !== confirmPassword) {
+        valid = false
+        toastr["error"]("Password not identic");
+
+    } else {
+        valid = true;
+        saveUserToLocalStorage();
     }
 
 
     if (dataNastere.value.trim() == "") {
+        validateInput(dataNastere);
+        valid = false;
         toastr["error"]("Date birth can't be blank");
-    }
-    if (valid) {
+    } else {
+        valid = true;
         saveUserToLocalStorage();
+
     }
-};
 
-function saveUserToLocalStorage() {
-    let userData = {
-        email: registerEmail.value.trim(),
-        password: registerPassword.value.trim(),
-    };
+}
 
-    let users = JSON.parse(localStorage.getItem("Users")) || [];
-    users.push(userData);
-    localStorage.setItem("Users", JSON.stringify(users));
 
-    alert("User registered successfully");
-    window.location.href = "proiect1_login";
 
 
 
 
 
+function saveUserToLocalStorage(user) {
+    // Convertim obiectul user într-un șir de caractere JSON
+    const userString = JSON.stringify(user);
 
+    // Salvăm șirul de caractere în localStorage
+    localStorage.setItem('user', userString);
+    window.location.href = "proiect1__login.html";
+    toastr["success"]("Register Succefull");
+}
 
 
+btnregister.addEventListener('click', btn_register);
 
-    // function saveUserToLocalStorage(username, email) {
-    //     try {
-    //         // Creăm un obiect pentru utilizator
-    //         const user = {
-    //             username: username,
-    //             email: email
-    //         };
-
-    //         // Convertim obiectul într-un șir JSON
-    //         const userJSON = JSON.stringify(user);
-
-    //         // Salvăm șirul JSON în local storage
-    //         localStorage.setItem('user', userJSON.stringify(user));
-
-    //         console.log('Utilizatorul a fost salvat în local storage.');
-    //     } catch (error) {
-    //         console.error('A apărut o eroare la salvarea utilizatorului:', error);
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // const username = 'john_doe';
-    // const email = 'john.doe@example.com';
-    // saveUserToLocalStorage(username, email);
-
-
-
-
-
-
-
-
-    // let user = [];
-
-
-
-    // if (valid input()) {
-
-    //     let stocare = localStorage.getItem("user-1")
-    //     if (stocare) {
-    //         user = JSON.parse(stocare)
-    //         let newUser(email, value, password...toate)
-    //     }
-
-
-    // }
-
-
-
-
-    // class User {
-    //     constructor(email, password, confpassword, firstname, name, bisrtdate) {
-    //         this.email = email;
-    //         this.password = password;
-    //         this.confpassword = confpassword;
-    //         this.firstname = firstbane;
-    //         this.name = name;
-    //         this.bisrtdate = birthdate;
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    toastr.options = {
-        "closeButton": false,
-        "debug": true,
-        "newestOnTop": false,
-        "progressBar": true,
-        "positionClass": "toast-top-center",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
+function btn_register() {
+    let valid = false
+    if (validateInput()) {
+        window.location.href = "proiect1__login.html";
+        saveUserToLocalStorage();
+    } else {
+        valid = false;
+        toastr["error"]("completeaza toate campurile");
     }
+}
+
+toastr.options = {
+    "closeButton": false,
+    "debug": true,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
 }
