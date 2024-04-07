8f71ddf6ae268a7be08fabdfc2daae0765a99923
GeorgeGal99 <144837092+GeorgeGal99@users.noreply.github.com>
2024-04-07 16:12:00 +0300

    ok

--- Proiect 22 mai/proiect1__login.js
+++ Proiect 22 mai/proiect1__login.js
@@ -1,5 +1,65 @@
+const loginEmail = document.querySelector("#emailInput");
+const loginPassword = document.querySelector("#passwordInput");
+let loginBtn = document.querySelector("#btn_login");
 
-let btn = document.querySelector("#btn__submit");
+
+
+loginBtn.addEventListener('click', function () {
+
+
+    let valid = true;
+
+    if (loginEmail.value.trim() === "") {
+        toastr["error"]("ups,something wrong!");
+        valid = false;
+    } else if (!validEmail(loginEmail.value.trim())) {
+        toastr["error"]("ups,something wrong!");
+        valid = false;
+    } else {
+        toastr["success"]("Login Successful", "Hello!")
+    }
+    if (loginPassword.value.trim() === "") {
+        toastr["error"]("ups,something wrong!");
+        valid = false;
+    } else if (loginPassword.value.length < 5) {
+        toastr["error"]("ups,something wrong!");
+        valid = false;
+    } else {
+        toastr["success"]("Login Successful", "Hello!")
+    }
+
+    if (valid) {
+        loginUser();
+    }
+
+});
+
+function validEmail(email) {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return emailRegex.test(email);
+}
+
+
+function loginUser() {
+    let users = JSON.parse(localStorage.getItem("user")) || [];
+    let email = loginEmail.value.trim();
+    let password = loginPassword.value.trim();
+
+    let userFound = users.find(
+        (user) => user.email === email && user.password === password
+    );
+
+    if (userFound) {
+        alert("Login successful");
+        localStorage.setItem("currentUserEmail", email);
+        // Redirecționează către pagina principală de căutare Google
+        window.location.href = "home_page.html";
+    } else {
+        alert("Invalid email or password. Please try again.");
+    }
+}
+
+
 
 toastr.options = {
     "closeButton": true,
@@ -18,8 +78,3 @@ toastr.options = {
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
 }
-btn.addEventListener('click', btn__submit());
-function btn__submit() {
-
-    // toastr["success"]("Login Successful", "Hello!")
-};
