a04928a94d52b5abda757efe1d3bd6542c92aad8
GeorgeGal99 <144837092+GeorgeGal99@users.noreply.github.com>
2024-04-09 20:01:29 +0300

    ok

--- Proiect 22 mai/proiect1_home.js
+++ Proiect 22 mai/proiect1_home.js
@@ -1,4 +1,20 @@
 
+function ShowCard(cardId) {
+    let card = document.getElementById(cardId);
+    card.style = "display:block";
+}
+
+function ToggleCard(cardId) {
+    let card = document.getElementById(cardId);
+    let display = window.getComputedStyle(card).display;
+    /*
+    if (display == "flex") {
+        card.style.display = "none"
+    } else {
+        card.style.display = "flex"
+    }*/
+    card.style.display = display == "flex" ? "none" : "flex";
+}
 
 
 
