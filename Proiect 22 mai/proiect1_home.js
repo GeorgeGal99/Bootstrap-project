ff2db7e377f6cfe4815f808bc1dae3488fdcaf7b
GeorgeGal99 <144837092+GeorgeGal99@users.noreply.github.com>
2024-04-09 20:44:36 +0300

    k

--- Proiect 22 mai/proiect1_home.js
+++ Proiect 22 mai/proiect1_home.js
@@ -7,14 +7,35 @@ function ShowCard(cardId) {
 function ToggleCard(cardId) {
     let card = document.getElementById(cardId);
     let display = window.getComputedStyle(card).display;
-    /*
+
     if (display == "flex") {
         card.style.display = "none"
     } else {
         card.style.display = "flex"
-    }*/
-    card.style.display = display == "flex" ? "none" : "flex";
+    }
+
+    //cod short hand
+    // card.style.display = display == "flex" ? "none" : "flex";
 }
+
+document.getElementById("addFlatForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    for (let i = 0; i < e.target.length; i++) {
+        if (e.target[i].type === "submit") {
+            continue
+        }
+        console.log(e.target[i].id + "-" + e.target[i].value);
+    }
+})
+
+/*
+function AddFlat(event) {
+    // prevent form submision (reloading page)
+    event.preventDefault();
+
+    console.log('add');
+}*/
 
 
 
